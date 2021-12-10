9e57e446ee237f10366099e6700cd2f561e88418
agumon69 <yusril.olan@gmail.com>
2021-12-10 20:01:38 +0800

    interact_lib

---
+++ Assets/Scripts/Sign Interaksi etc/PopupMap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupMap : MonoBehaviour
+{
+    [SerializeField] public GameObject dialogImage;
+    [SerializeField] public GameObject panel;
+    [SerializeField] int letterPerSecond;
+    public string dialog;
+    public bool PlayerInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && PlayerInRange)
+        {
+            if (panel.activeInHierarchy)
+            {
+                panel.SetActive(false);
+                dialogImage.SetActive(false);
+                
+            }
+            else
+            {
+                panel.SetActive(true);
+                dialogImage.SetActive(true);
+                
+
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInRange = false;
+            panel.SetActive(false);
+            dialogImage.SetActive(false);
+                
+        }
+    }
+}
---
+++ Assets/Scripts/Sign Interaksi etc/PopupStruk.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupStruk : MonoBehaviour
+{
+    [SerializeField] public GameObject dialogImage;
+    [SerializeField] public GameObject panel;
+    [SerializeField] public GameObject dialogImagi;
+    public bool PlayerInRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && PlayerInRange)
+        {
+            if (panel.activeInHierarchy)
+            {
+                panel.SetActive(false);
+                dialogImage.SetActive(false);
+                dialogImagi.SetActive(false);
+                
+            }
+            else
+            {
+                panel.SetActive(true);
+                dialogImage.SetActive(true);
+                dialogImagi.SetActive(true);
+                
+
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerInRange = false;
+            panel.SetActive(false);
+            dialogImage.SetActive(false);
+            dialogImagi.SetActive(false);
+                
+        }
+    }
+}
